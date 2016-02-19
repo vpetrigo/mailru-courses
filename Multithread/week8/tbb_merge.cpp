156c140e93eb68b092a3127fcb02eb8bef94edc5
Vladimir Petrigo <taenaru@gmail.com>
2016-02-19 20:33:21 +0300

    some clean up in the parallel mergesort problem

--- Multithread/week8/tbb_merge.cpp
+++ Multithread/week8/tbb_merge.cpp
@@ -1,10 +1,16 @@
+/**
+ * Author: vpetrigo
+ * Task:
+ * В последнем учебном модуле домашнее задание одно, предельно простое и дает сразу 100 баллов :)
+ * Используя OpenMP, Intel TBB или MPI (на ваш выбор) сделайте (у себя, локально) параллельную сортировку слиянием и отсортируйте полученный набор данных.
+ */
+
 #include <iostream>
 #include <iterator>
 #include <vector>
 #include <algorithm>
 #include <cstddef>
 #include <fstream>
-#include "tbb/parallel_for.h"
 #include "tbb/parallel_invoke.h"
 
 void Merge(int *data, int left, int mid, int right) {
@@ -49,21 +55,17 @@ int main() {
   const std::string filename{"dataset_12656_4.txt"};
   std::ifstream ifs{filename};
   std::ofstream ofs{"out.txt"};
-  std::ofstream ofs_fine{"fine.txt"};
   constexpr std::size_t arr_size= 1e5;
   std::size_t elem_q = 0;
   int array[arr_size];
-  int array2[arr_size];
 
   int elem;
 
   while (ifs >> elem) {
     array[elem_q++] = elem;
-    array2[elem_q - 1] = array[elem_q - 1];
   }
 
   MergeSort(array, 0, elem_q - 1);
-  std::sort(array2, array2 + elem_q);
 
   for (std::size_t i = 0; i < elem_q; ++i) {
     ofs << array[i];
@@ -72,14 +74,6 @@ int main() {
     }
   }
   ofs << std::endl;
-
-  for (std::size_t i = 0; i < elem_q; ++i) {
-    ofs_fine << array2[i];
-    if (i + 1 != elem_q) {
-      ofs_fine << ' ';
-    }
-  }
-  ofs_fine << std::endl;
 
   return 0;
 }
