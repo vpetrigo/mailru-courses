700160d917c101ad1d3aa8593c81fa179c708aaa
Vladimir Petrigo <taenaru@gmail.com>
2016-02-27 18:03:47 +0300

    Very dirty HTTP server implementation with 200 and 400 responses

--- Multithread/week9/http_makefile.mk
+++ Multithread/week9/http_makefile.mk
@@ -1,17 +1,21 @@
 CXX = clang++
 CC = clang
 SRC = http_server.cpp
-C_SRC = "D:\PVE\Projects\http-parser\"
+PARSER_SRC = "..\..\..\http-parser\http_parser.c"
+PARSER_OBJ = "..\..\..\http-parser\http_parser.o"
 OBJ = http_server.o http_parser.o
+LIB_PATH = "..\..\..\libuv"
+LIBUV_INC_DIR = "..\..\..\libuv\include"
+PARSER_INC_DIR = ""..\..\..\http_parser"
 
 all: $(OBJ)
-	clang++ -o http_server.exe $(OBJ) -L "D:\PVE\Projects\libuv" -luv -lws2_32 -lpsapi -liphlpapi -luserenv
+	clang++ -o http_server.exe $(OBJ) -L $(LIB_PATH) -luv -lws2_32 -lpsapi -liphlpapi -luserenv
 
 http_server.o: http_server.cpp
-	$(CXX) -I "D:\PVE\Projects\libuv\include" -I "D:\PVE\Projects\http-parser" $< -c -std=c++14 -DDEBUG
+	$(CXX) -I $(PARSER_INC_DIR) -I $(LIBUV_INC_DIR) $< -c -std=c++14 -DDEBUG
 
 http_parser.o:
-	$(CC) $(C_SRC)$< -c -std=c11
+	$(CC) -c $(PARSER_SRC) -std=c11
 
 clean:
 	rm -f *.o
--- Multithread/week9/http_server.cpp
+++ Multithread/week9/http_server.cpp
@@ -2,6 +2,7 @@
 #include <string>
 #include <sstream>
 #include <iomanip>
+#include <fstream>
 #ifdef _WIN32
   #include <winsock2.h>
 #else
@@ -17,27 +18,31 @@
   }
 
 #define LOG(msg) \
-  std::cout << msg << std::endl;
+  std::cout << msg << std::endl
+
+const std::string http_404 {"HTTP/1.0 404 Not Found\r\nContent-Type: text/html\r\n\r\n<b>Bad request</b>\r\n"};
 
 class HTTP_request_parser {
 public:
   HTTP_request_parser(uv_tcp_t *stream) {
+    http_parser_settings_init(&parser_settings);
     http_parser_init(&parser, HTTP_REQUEST);
     parser_settings.on_url = on_url;
     parser_settings.on_body = on_body;
     parser_settings.on_message_begin = on_message_begin;
+    parser_settings.on_message_complete = on_message_complete;
     parser_settings.on_header_field = on_header_field;
     parser_settings.on_header_value = on_header_value;
     parser_settings.on_headers_complete = on_headers_complete;
     parser.data = stream;
   }
 
-  http_parser *get_parser() noexcept {
-    return &parser;
+  http_parser& get_parser() noexcept {
+    return parser;
   }
 
-  http_parser_settings *get_parser_settings() noexcept {
-    return &parser_settings;
+  http_parser_settings& get_parser_settings() noexcept {
+    return parser_settings;
   }
 
 private:
@@ -49,20 +54,16 @@ private:
     return 0;
   }
 
-  static int on_url(http_parser *hp, const char *at, size_t length) {
-    std::cout << "url: " << std::string{at, length} << std::endl;
-
-    return 0;
-  }
+  static int on_url(http_parser *hp, const char *at, size_t length);
 
   static int on_header_field(http_parser *hp, const char *at, size_t length) {
-    std::cout << "Header field: " << std::string{at, length} << std::endl;
+    //std::cout << "Header field: " << std::string{at, length} << std::endl;
 
     return 0;
   }
 
   static int on_header_value(http_parser *hp, const char *at, size_t length) {
-    std::cout << "Header value: " << std::string{at, length} << std::endl;
+    //std::cout << "Header value: " << std::string{at, length} << std::endl;
 
     return 0;
   }
@@ -77,15 +78,15 @@ private:
     return 0;
   }
 
-  static int on_message_complete(http_parser *hp) {
-    std::cout << "msg parsed" << std::endl;
-    return 0;
-  }
+  static int on_message_complete(http_parser *hp);
 };
 
 class HTTP_client {
 public:
-  HTTP_client() : rp{&handle} {}
+  HTTP_client() : rp{&handle}, path{} {
+    handle.data = this;
+    write_req.data = &handle;
+  }
 
   HTTP_client(const HTTP_client& c) = delete;
   HTTP_client& operator=(const HTTP_client& c) = delete;
@@ -94,15 +95,114 @@ public:
     return &handle;
   }
 
-  void parse(const char *buf, size_t len) {
-      http_parser_execute(rp.get_parser(), rp.get_parser_settings(), buf, len);
+  void parse(char *buf, ssize_t len) {
+    auto parser_bytes = http_parser_execute(&rp.get_parser(), &rp.get_parser_settings(), buf, len);
+
+    if (len != parser_bytes) {
+      throw std::runtime_error{"Wrong number of bytes parsed"};
+    }
+  }
+
+  template <typename Path>
+  void set_path(Path&& p) noexcept {
+    path = std::move(p);
+  }
+
+  const std::string& get_path() const noexcept {
+    return path;
+  }
+
+  uv_write_t& get_write_req() noexcept {
+    return write_req;
   }
 
 private:
   uv_tcp_t handle;
   uv_write_t write_req;
   HTTP_request_parser rp;
+  std::string path;
 };
+
+int HTTP_request_parser::on_url(http_parser *hp, const char *at, size_t length) {
+  struct http_parser_url url_p;
+  // clean up new structure
+  http_parser_url_init(&url_p);
+  int status = http_parser_parse_url(at, length, false, &url_p);
+  CHECK(status, "cannot parse url");
+
+  HTTP_client *cptr = reinterpret_cast<HTTP_client *> (reinterpret_cast<uv_tcp_t *> (hp->data)->data);
+
+  LOG("URL: " + std::string (at, length));
+  if (url_p.field_set & (1 << UF_QUERY)) {
+    cptr->set_path("/");
+  }
+  else {
+    cptr->set_path(std::string{at + url_p.field_data[UF_PATH].off, url_p.field_data[UF_PATH].len});
+    std::cout << cptr->get_path() << std::endl;
+  }
+
+  return 0;
+}
+
+void close_after_wr(uv_handle_t *handle) {
+  HTTP_client *cptr = reinterpret_cast<HTTP_client *> (handle->data);
+
+  delete cptr;
+}
+
+void shutdown_after_wr(uv_shutdown_t *req, int status) {
+  uv_close(reinterpret_cast<uv_handle_t *> (req->handle), close_after_wr);
+  delete req;
+}
+
+void on_write(uv_write_t* req, int status) {
+  std::cout << "Write finished" << std::endl;
+  uv_shutdown_t *shutdown_req = new uv_shutdown_t;
+  uv_stream_t *peer = reinterpret_cast <uv_stream_t *> (req->data);
+
+  if (!uv_is_closing(reinterpret_cast<uv_handle_t *> (peer))) {
+    uv_shutdown(shutdown_req, peer, shutdown_after_wr);
+  }
+}
+
+void render_resp(uv_work_t *req) {
+  HTTP_client *cptr = reinterpret_cast<HTTP_client *> (req->data);
+  uv_stream_t *client_stream = reinterpret_cast<uv_stream_t *> (cptr->get_handle());
+  uv_buf_t buf;
+  std::string filepath = "." + cptr->get_path();
+  if (cptr->get_path() == "/" || access(filepath.c_str(), R_OK)) {
+    buf = uv_buf_init(const_cast<char *> (http_404.c_str()), http_404.size());
+  }
+  else {
+    std::cout << "Exists" << std::endl;
+    std::ifstream ifs{filepath, std::ios::ate | std::ios::binary};
+    std::string filedata(ifs.tellg(), '\0');
+    // rewind at the begining
+    ifs.seekg(0);
+    filedata.assign(std::istreambuf_iterator<char> {ifs}, std::istreambuf_iterator<char> {});
+    std::string http_200 {"HTTP/1.0 200 OK\r\nContent-Type: text/html\r\nConnection: keep-alive\r\n"};
+    http_200.append("Content-Length: " + std::to_string(filedata.size()) + "\r\n");
+    http_200.append("Access-Control-Allow-Origin: *\r\n\r\n");
+    buf = uv_buf_init(const_cast<char *> ((http_200 + filedata).c_str()), (http_200 + filedata).size());
+  }
+  int status = uv_write(&cptr->get_write_req(), client_stream, &buf, 1, on_write);
+  CHECK(status, "write cannot be done");
+}
+
+void after_render(uv_work_t *req, int status) {
+  delete req;
+}
+
+int HTTP_request_parser::on_message_complete(http_parser *hp) {
+  HTTP_client *cptr = reinterpret_cast<HTTP_client *> (reinterpret_cast<uv_tcp_t *> (hp->data)->data);
+  uv_work_t *render_req = new uv_work_t;
+
+  render_req->data = cptr;
+  int status = uv_queue_work(cptr->get_handle()->loop, render_req, render_resp, after_render);
+  CHECK(status, "queue cannot be started");
+
+  return 0;
+}
 
 class HTTP_server {
 public:
@@ -152,7 +252,6 @@ private:
 
     if (nread >= 0) {
       // get HTTP request and parse it
-      std::cout << buf->base << std::endl;
       cptr->parse(buf->base, nread);
     }
     else {
